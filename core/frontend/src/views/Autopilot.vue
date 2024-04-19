75058647bd0a162e4b640a058869bcacd90abf6e
sakthivelj <sakthivel1023@gmail.com>
2024-04-19 10:06:53 +0530

    coratia change

--- core/frontend/src/views/Autopilot.vue
+++ core/frontend/src/views/Autopilot.vue
@@ -143,11 +143,11 @@ export default Vue.extend({
         version = `${this.firmware_info.version} (${this.firmware_info.type})`
       }
 
-      const record: Record<string, string> = {
-        'Board name': this.current_board?.name ?? 'Unknown',
-        Manufacturer: this.current_board?.manufacturer ?? 'Unknown',
-        'Mavlink platform': this.current_board?.platform ?? 'Unknown',
-        'Firmware version': version,
+    const record: Record<string, string> = {
+        // 'Board name': this.current_board?.name ?? 'Unknown',
+        // Manufacturer: this.current_board?.manufacturer ?? 'Unknown',
+        // 'Mavlink platform': this.current_board?.platform ?? 'Unknown',
+        // 'Firmware version': version,
         'Vehicle type': this.vehicle_type ?? 'Unknown',
       }
 
